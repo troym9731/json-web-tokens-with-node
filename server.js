b6e4ae3b98878e8c637d16fce33ba8b267d15062
Troy Mullaney <troy9731@gmail.com>
2015-12-10 15:39:44 -0700

    Final commit. Finished tutorial

--- server.js
+++ server.js
@@ -31,8 +31,71 @@ app.get('/setup', function(req, res) {
 
   nick.save(function(err) {
     if (err) throw err;
+
+    console.log('User saved successfully');
+    res.json({ success: true });
   });
 });
+
+var apiRoutes = express.Router();
+
+apiRoutes.post('/authenticate', function(req, res) {
+  User.findOne({
+    name: req.body.name
+  }, function(err, user) {
+    if (err) throw err;
+
+    if (!user) {
+      res.json({ success: false, message: 'Authentication failed. User not found.' });
+    } else if (user) {
+      if (user.password != req.body.password) {
+        res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+      } else {
+        var token = jwt.sign(user, app.get('superSecret'), {
+          expiresInMinutes: 1440
+        });
+
+        res.json({
+          success: true,
+          message: 'Enjoy your token!',
+          token: token
+        });
+      }
+    }
+  });
+});
+
+apiRoutes.use(function(req, res, next) {
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+  if (token) {
+    jwt.verify(token, app.get('superSecret'), function(err, decoded) {
+      if(err) {
+        return res.json({ success: false, message: 'Failed to authenticate token.' });
+      } else {
+        req.decoded = decoded;
+        next();
+      }
+    });
+  } else {
+    return res.status(403).send({
+      success: false,
+      message: 'No token provided.'
+    });
+  }
+});
+
+apiRoutes.get('/', function(req, res) {
+  res.json({ message: 'Welcome to the coolest API on earth!' });
+});
+
+apiRoutes.get('/users', function(req, res) {
+  User.find({}, function(err, users) {
+    res.json(users);
+  });
+});
+
+app.use('/api', apiRoutes);
 
 app.listen(port);
 console.log('Magic happens at http:localhost:' + port);
